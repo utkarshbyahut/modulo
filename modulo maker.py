3c9e71cc5be75bfaaccd3dee9e5b4ce6fd3e6396
utkarshbyahut <utkarshbyahut16@gmail.com>
2024-07-10 12:42:40 -0700

    Commit 2 : FInalizing a Working Modulo Algorithm

--- modulo maker.py
+++ modulo maker.py
@@ -1,16 +1,23 @@
-# Computing the modulo of 2 Integers
+# Computing the modulo (%) (Remainder) of 2 Integers
 
-# Input the 2 integers into the console 
+# Input the 2 integers into the console assuming inputs are not 0
 intInput1 = int(input("Number 1 : "))
 intInput2 = int(input("Number 2 : "))
 
-# Running a while loop to check when intInput1 is not equal to intInput2
-while (intInput2 != intInput2):
+def modulo(input1, input2):
+    marker = 0 # Flag to check case, set to default for Case 2
 
-    if (intInput2 < intInput1):
-        intInput1 -= intInput2
+    # Case 1 : Input1 > Input 2
+    while (input2 < input1):
+        input1 -= input2 
+        marker = 1 # Flag to mark as case 1, so case 2 gets ommited
+        modulus = input1
 
-    if (intInput1 < intInput2):
-        intInput2 -= intInput1
+    # Case 2 : Input1 < Input 2
+    while ((input1 < input2) and (marker == 0)):
+        input2 -= input1
+        modulus = input2
 
-print(f"Mod is {intInput1}")
+    print("Mod is 0") if (input1 == input2) else print(f"Mod is {modulus}")
+
+modulo(intInput1, intInput2)
